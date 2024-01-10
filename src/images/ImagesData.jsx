0ad0fe09d2d82e2db4cb2309d065235f09594fce
nimeshakya <nimeshakyabtw@gmail.com>
2024-01-10 19:43:24 +0545

    added posters for movies and some random users

--- src/images/ImagesData.jsx
+++ src/images/ImagesData.jsx
@@ -1,26 +1,26 @@
 export const pop_week_movies = [
     {
-        img: new URL('/assets/pop_week_movies/mv1.png', import.meta.url).href,
-        description: 'movie: nope',
-    },
-    {
-        img: new URL('/assets/pop_week_movies/mv2.png', import.meta.url).href,
-        description: 'movie: flipped',
-    },
-    {
-        img: new URL('/assets/pop_week_movies/mv3.png', import.meta.url).href,
-        description: 'movie: the others',
-    },
-    {
-        img: new URL('/assets/pop_week_movies/mv4.png', import.meta.url).href,
+        img: new URL('/assets/movie_posters/mv5.png', import.meta.url).href,
         description: 'movie: lal singh chadda',
     },
     {
-        img: new URL('/assets/pop_week_movies/mv5.png', import.meta.url).href,
+        img: new URL('/assets/movie_posters/mv6.png', import.meta.url).href,
         description: 'movie: incantation',
     },
     {
-        img: new URL('/assets/pop_week_movies/mv6.png', import.meta.url).href,
+        img: new URL('/assets/movie_posters/mv7.png', import.meta.url).href,
+        description: 'movie: the others',
+    },
+    {
+        img: new URL('/assets/movie_posters/mv8.png', import.meta.url).href,
         description: 'movie: sir',
+    },
+    {
+        img: new URL('/assets/movie_posters/mv9.png', import.meta.url).href,
+        description: 'movie: flipped',
+    },
+    {
+        img: new URL('/assets/movie_posters/mv10.png', import.meta.url).href,
+        description: 'movie: nope',
     },
 ];
