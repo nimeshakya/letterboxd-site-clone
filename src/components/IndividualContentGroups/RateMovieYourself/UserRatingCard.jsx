3efb0675f33eb9c3927f021f6dab8602fbc63648
nimeshakya <nimeshakyabtw@gmail.com>
2024-01-17 17:35:37 +0545

    stars in rating is handled in state (change on hover in future)

--- src/components/IndividualContentGroups/RateMovieYourself/UserRatingCard.jsx
+++ src/components/IndividualContentGroups/RateMovieYourself/UserRatingCard.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { FaRegStar } from 'react-icons/fa';
 
 import MovieTypeDefiner from '../../MovieTypeDefiner/MovieTypeDefiner';
@@ -6,13 +7,25 @@ import MovieTypeDefiner from '../../MovieTypeDefiner/MovieTypeDefiner';
 import './UserRatingCard.scss';
 
 const UserRatingCard = ({ movie }) => {
-    const renderRatingStars = () => {
-        const stars = [];
-        for (let i = 0; i < 5; ++i) {
-            stars.push(<FaRegStar key={i} className='rating-stars' />);
+    const [ratingStars, setRatingStars] = useState([]);
+
+    useEffect(() => {
+        if (ratingStars.length < 5) {
+            for (let i = 0; i < 5; ++i) {
+                setRatingStars((ratingStars) => {
+                    return [
+                        ...ratingStars,
+                        <FaRegStar key={i} className='rating-stars' />,
+                    ];
+                });
+            }
         }
-        return stars;
+    }, []);
+
+    const handleRatingClick = (e) => {
+        e.preventDefault();
     };
+
     return (
         <div className='user-rating-card'>
             <div className='rating-desc-container'>
@@ -26,7 +39,13 @@ const UserRatingCard = ({ movie }) => {
                     <p>{movie.desc}</p>
                 </div>
             </div>
-            <div className='rating-stars-container'>{renderRatingStars()}</div>
+            <Link
+                to='/'
+                onClick={handleRatingClick}
+                className='rating-stars-container'
+            >
+                {ratingStars}
+            </Link>
         </div>
     );
 };
