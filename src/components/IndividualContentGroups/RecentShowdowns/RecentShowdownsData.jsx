219d8401e56bb3f03be85c5c5b6e347f98a6b820
nimeshakya <nimeshakyabtw@gmail.com>
2024-06-16 19:58:13 +0545

    Adjustment and fixing iteration numero quince.

--- src/components/IndividualContentGroups/RecentShowdowns/RecentShowdownsData.jsx
+++ src/components/IndividualContentGroups/RecentShowdowns/RecentShowdownsData.jsx
@@ -5,17 +5,20 @@ export const recentShowdownsData = [
         img: new URL('/assets/movie_posters/mv25.png', import.meta.url).href,
 
         description: 'One of the Best Science Fiction Movie of the decade.',
+        type: 'in progress',
     },
     {
         movie: 'Flipped',
         year: '2012',
         img: new URL('/assets/movie_posters/mv9.png', import.meta.url).href,
         description: 'One of the Best Science Fiction Movie of the decade.',
+        type: 'new',
     },
     {
         movie: 'Lal Singh Chadda',
         year: '2017',
         img: new URL('/assets/movie_posters/mv5.png', import.meta.url).href,
         description: 'Adapting Forest Gump is quite risky as it requires gut.',
+        type: '',
     },
 ];
